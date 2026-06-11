073e1a700110e0b9f805dc09a49b982165b53ecf
maxipdev <maxipena.dev@gmail.com>
2026-06-11 00:04:06 -0300

    cambios en las politicas CORS

--- api/midelwares/cors.js
+++ api/midelwares/cors.js
@@ -1,22 +1,24 @@
-import cors from 'cors'
+import cors from "cors";
 
 const ACCEPTED_ORIGINS = [
-  'http://localhost:5173',
-  'http://localhost:1234',
-  'https://jimbofertas.vercel.app'
-]
+  "http://localhost:5173",
+  "http://localhost:1234",
+  "https://jimbofertas.vercel.app",
+  "https://estacionsur.vercel.app",
+];
 
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
-  origin: (origin, callback) => {
-    if (acceptedOrigins.includes(origin)) {
-      return callback(null, true)
-    }
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
+  cors({
+    origin: (origin, callback) => {
+      if (acceptedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
 
-    if (!origin) {
-      return callback(null, true)
-    }
+      if (!origin) {
+        return callback(null, true);
+      }
 
-    return callback(new Error('Not allowed by CORS'))
-  },
-  credentials: true
-})
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  });
